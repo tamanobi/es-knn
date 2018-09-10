25fd89406a4f32f5db2755490da6a956569067a5
Yasu <yasu@pixiv.co.jp>
2018-09-11 02:37:23 +0900

    コマンドラインからデモができる

--- cli.py
+++ cli.py
@@ -11,40 +11,13 @@ import requests
 ESHOSTS=cycle(['http://127.0.0.1:9200'])
 
 def images(es_index, es_type, es_id):
-    # Parse elasticsearch ID. If "demo", pick a random demo image ID.
-    if es_id.lower() == "demo":
-        es_id = random.choice(DEMO_IDS)
-
-    elif es_id.lower() == "random":
-        body = {
-            "_source": ["path"],
-            "size": 1,
-            "query": {
-                "function_score": {
-                    "query": {"match_all": {}},
-                    "boost": 5,
-                    "random_score": {},
-                    "boost_mode": "multiply"
-                }
-            }
-        }
-        req_url = "%s/%s/%s/_search" % (next(ESHOSTS), es_index, es_type)
-        req = requests.put("%s/%s" % (next(ESHOSTS), 'images'))
-        req = requests.get(req_url, json=body)
-        es_id = req.json()["hits"]["hits"][0]["_id"]
-
-    # Get number of docs in corpus.
-    req_url = "%s/%s/%s/_count" % (next(ESHOSTS), es_index, es_type)
-    req = requests.get(req_url)
-    count = req.json()["count"]
-
-    # Get the nearest neighbors for the query image, which includes the image.
-    req_url = "%s/%s/%s/%s/_aknn_search?k1=300&k2=5" % (
-        next(ESHOSTS), es_index, es_type, '1')
-    req = requests.get(req_url)
+    req = requests.get(f'{next(ESHOSTS)}/{es_index}/{es_type}/search0000/_aknn_search?k1=500&k2=10')
     hits = req.json()["hits"]["hits"]
     took_ms = req.json()["took"]
     query_img, neighbor_imgs = hits[0], hits[1:]
+
+    res = requests.get(f'{next(ESHOSTS)}/{es_index}/{es_type}/search0000')
+    print(res.text)
 
     print(took_ms, query_img, neighbor_imgs)
 
@@ -71,10 +44,37 @@ def get_feature(img:Image) -> numpy.ndarray:
     return numpy.vectorize(bool_to_float)(h.hash.flatten())
 
 if __name__ == '__main__':
-    # args = get_args()
-    # input_image_path = Path(args['input_image_path'])
-    # img = Image.open(str(input_image_path))
-    # feature = get_feature(img)
+    args = get_args()
+
+    input_image_path = Path(args['input_image_path'])
+    img = Image.open(str(input_image_path))
+    print(input_image_path)
+    feature = get_feature(img)
+
+    search_id = 'search0000'
+    aknn_doc = {
+        "_id": search_id,
+        "_source": {
+            "path": '',
+            "_aknn_vector": feature.tolist()
+        }
+    }
+
+    index_name = 'images'
+    doc_type = 'image'
+    body = {
+        "_index": index_name,
+        "_type": doc_type,
+        "_aknn_uri": f"aknn_models/aknn_model/{index_name}",
+        "_aknn_docs": [aknn_doc]
+    }
+
+    host = args['es_hosts']
+    post_url = f"{host}/_aknn_index"
+    res = requests.post(post_url, json=body)
+    print(res.text)
     images('images', 'image', 'random')
+    res = requests.delete(f'{host}/{index_name}/{doc_type}/{search_id}')
+    print(res.text)
 
 
